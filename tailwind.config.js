7be9d95a523890c15c22cee9fb23fae7f89142e7
劉展宏 <>
2023-05-17 16:25:17 +0800

    no message

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ export default {
 
     theme: {
         extend: {
+            colors:{
+                       transparent:'transparent',
+                       
+                        current: 'currentColor',
+                        black:colors.black,
+                        white:colors.white,
+                        gray:colors.coolGray, 
+                        'bg-gray-background':'#f7f8fc',       
+                        }, 
+
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
